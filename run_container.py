a564f02207da18525ebea85a3e3e4d786825a98d
sergivalverde <sergivalv@gmail.com>
2020-04-11 09:00:34 +0200

    added COVID inference

--- run_container.py
+++ run_container.py
@@ -1,31 +1,52 @@
 import argparse
 import pyxnat
 import os
-import shutil
+import pydicom
+from inference.run_inference import run_inference
+
 
 def main(args):
     xnat_host = os.environ['XNAT_HOST']
     xnat_user = os.environ['XNAT_USER']
     xnat_pass = os.environ['XNAT_PASS']
 
-    
     # Find the ID of each scan as well as the corresponding DICOMS
     scan_ids = os.listdir('/input/SCANS')
     scan_dicoms = []
     for scan_id in scan_ids:
         scan_base = '/input/SCANS/{}/DICOM/'.format(scan_id)
-        scan_dicoms += [os.path.join(scan_base, x) for x in os.listdir(scan_base) if x.endswith('.dcm')] #[0] SELECT ONLY ONE DCM!
+        scan_dicoms += [os.path.join(scan_base, x)
+                        for x in os.listdir(scan_base)
+                        if x.endswith('.dcm')]  # [0] SELECT ONLY ONE DCM!
 
     # Get one covid probability for each scan_id (deep learning much?)
-    covid_probs = [65.0, 50.0]
+        # compute the probability for the current scan to COVID19
+    covid_probs = []
+
+    for scan in scan_dicoms:
+        # load the dcm to RGB
+        input_dcm = pydicom.dcmread(scan_dicoms)
+        input_scan = input_dcm.pixel_array
+
+        # run inference in the current scan
+        prob = run_inference(input_scan)
+        covid_probs.append(prob)
 
     # Write results as a note to each scan
-    session = pyxnat.Interface(server=xnat_host, user=xnat_user, password=xnat_pass)
-    for scan_id, dicom_filepath, covid_prob in zip(scan_ids, scan_dicoms, covid_probs):
+    session = pyxnat.Interface(server=xnat_host,
+                               user=xnat_user,
+                               password=xnat_pass)
+
+    for scan_id, dicom_filepath, covid_prob in zip(scan_ids,
+                                                   scan_dicoms,
+                                                   covid_probs):
         scan = session.select('/projects/{}/subjects/{}/experiments/{}/scans/{}'.format(
             args.project, args.subject, args.experiment, scan_id))
-        scan.attrs.set('xnat:imageScanData/note', 'p(COVID) {}% at {}'.format(covid_prob, dicom_filepath))
+        scan.attrs.set('xnat:imageScanData/note', 'p(COVID) {}% at {}'.format(
+            covid_prob,
+            dicom_filepath))
     session.disconnect()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract data from XNAT.')
@@ -35,9 +56,3 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
     main(args)
-
-
-
-
-
-
