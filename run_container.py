8aafe42494fb2484e98a0ab8451e2b0baedba153
xnat-nic <xnat@nic.udg.edu>
2020-04-20 18:02:09 +0200

    Fix problem with wrong email recipient.

--- run_container.py
+++ run_container.py
@@ -1,95 +1,98 @@
 import argparse
-
 from email.mime.text import MIMEText
 import smtplib
-
 import os
 from pyxnat import Interface
 from inference.run_inference import run_inference
 
 
 def main(args):
-    xnat_host = os.environ['XNAT_HOST']
-    xnat_user = os.environ['XNAT_USER']
-    xnat_pass = os.environ['XNAT_PASS']
-
-    print(os.environ)
+    # =====================================================================
+    # INFERENCE
+    # =====================================================================
 
     # define model to used
     model = 'resnet50_512'
-
-    # print some loggin information
-    print('--------------------------------------------------')
-    print('model name:', model)
-    print('--------------------------------------------------')
-    print(' ')
+    print('model name:', model) # print some loggin information
 
     # Find the ID of each scan as well as the corresponding DICOMS
-    scan_ids = os.listdir('/input/SCANS')
-
-    scan_dicoms = []
+    scan_ids, scan_dicoms = os.listdir('/input/SCANS'), []
     for scan_id in scan_ids:
-        scan_base = '/input/SCANS/{}/DICOM/'.format(scan_id)
-        current_scan_dicoms = [os.path.join(scan_base, x)
-                        for x in os.listdir(scan_base)
-                        if x.endswith('.dcm')]  # [0] SELECT ONLY ONE DCM!
+        base = '/input/SCANS/{}/DICOM/'.format(scan_id)
+        current_scan_dicoms = [
+            os.path.join(base, x) for x in os.listdir(base) if x.endswith('.dcm')]  
         if len(current_scan_dicoms) > 1:
             current_scan_dicoms = current_scan_dicoms[0]
-
         scan_dicoms += current_scan_dicoms
+    
+    print('Performing inference...')
+    covid_probs = [run_inference(scan) for scan in scan_dicoms]
 
+    # =====================================================================
+    # RESULTS
+    # =====================================================================
 
-    print('--------------------------------------------------')
-    print('Inference')
-    covid_probs = []
-    for scan in scan_dicoms:
-        # perform inference
-        prob = run_inference(scan)
-        covid_probs.append(prob)
-    print('--------------------------------------------------')
+    #### Send results to appropiate places
+    session = Interface(server=os.environ['XNAT_HOST'], 
+        user=os.environ['XNAT_USER'], password=os.environ['XNAT_PASS'])
 
-    # Write results as a note to each scan
-    session = Interface(server=xnat_host,
-                        user=xnat_user,
-                        password=xnat_pass)
+    # ---------------------------------------------------------------------
+    # Write note on XNAT scan with the results
+    # ---------------------------------------------------------------------
+    try:
+        for scan_id, dicom_filepath, covid_prob in zip(scan_ids, scan_dicoms, covid_probs):
+            scan = session.select('/projects/{}/subjects/{}/experiments/{}/scans/{}'.format(
+                args.project, args.subject, args.experiment, scan_id))
+            scan.attrs.set('xnat:imageScanData/note', 'p(COVID): {0:.2f}%'.format(covid_prob))
 
-    for scan_id, dicom_filepath, covid_prob in zip(scan_ids,
-                                                   scan_dicoms,
-                                                   covid_probs):
-        print('--------------------------------------------------')
-        scan = session.select('/projects/{}/subjects/{}/experiments/{}/scans/{}'.format(
-            args.project, args.subject, args.experiment, scan_id))
-        scan.attrs.set('xnat:imageScanData/note',
-                       'p(COVID): {0:.2f}%'.format(covid_prob))
-        print('DICOM PATH', dicom_filepath)
-        print("SCAN ID:", scan_id)
-        print("predicted probability:", covid_prob)
-        print('--------------------------------------------------')
+            print('--------------------------------------------------')
+            print('DICOM PATH', dicom_filepath)
+            print("SCAN ID:", scan_id)
+            print("predicted probability:", covid_prob)
+            print('--------------------------------------------------')
+    except Exception as e:
+        session.disconnect()
+        raise RuntimeError('Error while writting results to XNAT', str(e))
 
-    # Get the actual username (XNAT_USER is a token alias)
-    # Then get user email to send results
-    username = session.get('/data/services/tokens/validate/{}/{}'.format(
-        os.environ['XNAT_USER'], os.environ['XNAT_PASS'])).json()['valid']
-    users_email = session.manage.users.email(username)
+    # ---------------------------------------------------------------------
+    # Send an email with results to user
+    # ---------------------------------------------------------------------
+    
+    #### Get the actual username and email (XNAT_USER is a token alias)
+    try:
+        # Get actual username from session token (admin if launched automatically)
+        username_token = session.get('/data/services/tokens/validate/{}/{}'.format(
+            os.environ['XNAT_USER'], os.environ['XNAT_PASS'])).json()['valid']
+        # Get actual username from user that uploaded experiment
+        username_upload = session.select('/projects/{}/subjects/{}/experiments/{}'.format(
+            args.project, args.subject, args.experiment)).attrs.get('insert_user')
+        # Preferably use token username (containers can be run by people other than uploader)
+        # If token username is admin, container may be automatically launched (so get uploader)
+        if username_token != 'admin': username = username_token
+        else: username = username_upload
+        # Then get user email to send results    
+        users_email = session.manage.users.email(username)
+    except Exception as e:
+        session.disconnect()
+        raise RuntimeError('Error while getting user email', str(e))
+    session.disconnect() # Interaction with XNAT server is finished
+
+    #### EMAIL sending
+    gmail_email = 'albertclerigues@gmail.com'
+    gmail_password = 'tqqvgothdjdjppxn'
 
     body = []
     for scan_id, dicom_filepath, covid_prob in zip(scan_ids, scan_dicoms, covid_probs):
-        body.append('--------------------------------------------------')
+        body.append('---------------------------------------------------------------')
         body.append('Subject ID: {}'.format(args.subject))
         body.append('DICOM Path: {}'.format(dicom_filepath))
         body.append('Scan ID: {}'.format(scan_id))
         body.append('Predicted Probability: {:.2f}%'.format(covid_prob))
-        body.append('--------------------------------------------------')
-    body = '\n'.join(body)
-
-    gmail_email = ''
-    gmail_password = ''
-    raise NotImplementedError('Please use your own gmail email and password :D')
-
-    email_text = MIMEText(body)
+        body.append('---------------------------------------------------------------')
+    email_text = MIMEText('\n'.join(body))
     email_text['From'] = gmail_email
     email_text['To'] = users_email
-    email_text['Subject'] = 'XNAT VICOROB - DEBUG! - COVID-19 probability results'
+    email_text['Subject'] = 'XNAT VICOROB - [DEBUG] - COVID-19 probability results'
 
     print('-' * 40 + 'Sending mail:' + '-' * 40)
     print(email_text.as_string())
@@ -99,10 +102,8 @@ def main(args):
     server.ehlo()
     server.login(gmail_email, gmail_password)
     server.sendmail(gmail_email, users_email, email_text.as_string())
-
-    # disconnect XNAT and email sessions
-    session.disconnect()
     server.close()
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Extract data from XNAT.')
